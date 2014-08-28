4128add105c6577fb4331f0d3e78e8a244b5b796
Luiz Rogerio <luizrogeriocn@gmail.com>
2014-08-28 00:12:42 -0300

    Small change to teste github notification headers

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -96,7 +96,7 @@ module.exports = function(app) {
 	app.post('/webhooks/github', function(req, res) {
 		console.log(req.body);
 		var time_now = new Date();
-		var name = req.headers.x-github-event;
+		var name = req.headers['x-github-event'];
 
 		request({
 		  uri: "https://colabore.slack.com/services/hooks/incoming-webhook?token=UFqe6mu7euTJPHHMGXJe7r3F",
